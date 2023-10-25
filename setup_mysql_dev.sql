85521ada0f0a3c424d7d547bc3c468d25eae6a1f
Worlasi15 <efyawalasi@gmail.com>
2023-10-25 16:41:20 -0700

    dev file

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,13 @@
+-- Create the hbnb_dev_db database if not in existence
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+--Create the hbnb user with a password if not in existence
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+--Grant all hbnb_dev_db privileges to hbnb_dev
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+--Grant SELECT privileges on performance_schema to hbnb_dev
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+FLUSH PRIVILEGES;
