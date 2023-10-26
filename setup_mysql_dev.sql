c1838038e00513403ae7933f3093fbdbe01f313f
Worlasi15 <efyawalasi@gmail.com>
2023-10-25 17:07:33 -0700

    server script

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,9 +1,3 @@
-/*script to do the following:
-  creates an AIRBNB_dev Database if not found
-  creates an hbnb user and password if not found
-  grants privileges and accesses from hbnb_dev database and performance_schema
-  to hbnb_dev localhost*/
-
 -- Create the hbnb_dev_db database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
@@ -15,5 +9,3 @@ GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
 -- Grant SELECT privilege on performance_schema to hbnb_dev
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
-
-FLUSH PRIVILEGES;
