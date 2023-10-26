cf2c1e7310555b4b1628be19d7b4b9e2ba548d14
Worlasi15 <efyawalasi@gmail.com>
2023-10-25 17:02:59 -0700

    script for server

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -15,3 +15,5 @@ GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
 -- Grant SELECT privilege on performance_schema to hbnb_dev
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+FLUSH PRIVILEGES;
